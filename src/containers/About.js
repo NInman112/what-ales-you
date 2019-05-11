a587678978b8587203a37a8e87d0bc2ece1b7d58
Nick Inman <ninman@Nicks-MBP.fios-router.home>
2019-05-11 00:09:09 -0700

    css and view changes

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import '../stylesheets/App.css';
 
 export default class About extends Component {
 
@@ -6,6 +7,10 @@ export default class About extends Component {
     return(
       <div>
         <div>
+          <a href='https://flatironschool.com' target='_blank'><img src='FS_wiki.png' width='30%'></img></a>
+          <p className='aboutPTag'>
+            This web application was built as a Flatiron React project and utilizes a backend Rails API which saves user data and a React frontend to provide the user interface.
+          </p>
           <p>
             Icons made by
             <a href="https://www.freepik.com/?__hstc=57440181.74d1d4c08743298030ee40bd3dd04231.1557436705945.1557436705945.1557436705945.1&__hssc=57440181.1.1557436705946&__hsfp=3218011592" title="Freepik">Freepik</a> from
