e084018f3e1a11178b2b455b8d41a652d05730db
Nick Inman <ninman@Nicks-MacBook-Pro.local>
2019-05-21 15:21:01 -0700

    updates

--- src/containers/DeleteFavorites.js
+++ src/containers/DeleteFavorites.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { deleteFavBeer } from '../actions/deleteFavBeer'
 import '../stylesheets/list.css';
 
-
 class DeleteFavorites extends Component {
 
   onClick = (e) => {
